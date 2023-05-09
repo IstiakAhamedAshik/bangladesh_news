3276fe1e52f1c96c317aca535d84b2478a8c585b
Istiak Ahamed Ashik <istiak1439@gmail.com>
2023-05-09 10:13:22 +0600

    setup firebase and reactRouter

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBXvkjHNw50Qvqd_WD-8Voy14eevrW84XQ',
+  authDomain: 'bangladesh-news-2cef8.firebaseapp.com',
+  projectId: 'bangladesh-news-2cef8',
+  storageBucket: 'bangladesh-news-2cef8.appspot.com',
+  messagingSenderId: '152379018334',
+  appId: '1:152379018334:web:9b247c1033042dc1a9b8cf',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export default app
